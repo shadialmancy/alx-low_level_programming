b23e1fca98ecfbb4c1d0f52d5eb9d7da9edacffc
shadialmancy <shadi.almancy.sa@gmail.com>
2023-03-16 14:37:17 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - program that display data with printf function
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
