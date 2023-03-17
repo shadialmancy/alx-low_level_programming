ecd6c87bfd2359ae74dc3ab7b5c830595c03a752
shadialmancy <shadi.almancy.sa@gmail.com>
2023-03-17 11:06:40 -0700

    done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* 
+ * auth : shadi ahmed 
+ *
+ *  main - Prints a random number and states whether its postive,negative or zero 
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
