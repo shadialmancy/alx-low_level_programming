212ea8e6da4d3aa5ed9a3add988d77728f1d452f
shadialmancy <shadi.almancy.sa@gmail.com>
2023-04-03 11:47:42 -0700

    done

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,23 +8,30 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int x;
-	int y = 0;
+	int x, y;
 
-	while (needle[y] != '\0')
-		y++;
-
-	while (*haystack)
+	if (needle[0] == '\0')
 	{
-		for (x = 0; needle[x]; i++)
-		{
-			if (haystack[x] != needle[x])
-				break;
-		}
-		if (x != y)
-			haystack++;
-		else
-			return (haystack);
+		return (haystack);
 	}
-	return (NULL);
+
+	for (x = 0; haystack[x] != '\0'; x++)
+	{
+		if (haystack[x] == needle[0])
+		{
+			for (y = 0; needle[y] != '\0'; y++)
+			{
+				if (haystack[x + y] != needle[y])
+				{
+					break;
+				}
+			}
+
+			if (needle[y] == '\0')
+			{
+				return (haystack + x);
+			}
+		}
+	}
+	return ('\0');
 }
